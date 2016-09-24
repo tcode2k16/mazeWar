e9eef9a30b371a7c84d6c0f14445a4b29606a254
tcode2k16 <tcode2k16@users.noreply.github.com>
2016-09-24 21:44:15 +0800

    Add files via upload

---
+++ Image.js
@@ -0,0 +1,28 @@
+/**
+ * Created by home on 13/10/15.
+ */
+
+var Monster_1 = new Image();
+Monster_1.src = "Monster_src/1.jpg";
+
+var Monster_2 = new Image();
+Monster_2.src = "Monster_src/2.jpg";
+
+var Monster_3 = new Image();
+Monster_3.src = "Monster_src/3.jpg";
+
+var Monster_4 = new Image();
+Monster_4.src = "Monster_src/4.jpg";
+
+var bullet_img = new Image();
+bullet_img.src = "Monster_src/bullet.png";
+
+var compass_img = new Image();
+compass_img.src = "Monster_src/compass.png";
+
+//var Monster = {
+//    "1": Image("Monster_src/1.jpg"),
+//    "2": Image("Monster_src/2.jpg"),
+//    "3": Image("Monster_src/3.jpg"),
+//    "4": Image("Monster_src/4.jpg")
+//}
